1cf13493fa5dd07e64aa04b4a0b2bc00c8053d1f
Spastic <junior.tan@live.com>
2023-02-07 16:24:05 +0800

    added car model, car controller, steering wheel rotation, mirrors

---
+++ My project/Assets/UpdatePos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePos : MonoBehaviour
+{
+    public GameObject car;
+    public Vector3 offset;
+    private GameObject driverHeadPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        driverHeadPos = car.transform.Find("DriverHeadPos").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = driverHeadPos.transform.position + offset;
+        transform.rotation = driverHeadPos.transform.rotation;
+    }
+}
