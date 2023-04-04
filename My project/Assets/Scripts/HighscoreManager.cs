e711cb5ff682153f9752bd8d0da616802695cd0a
Spastic <junior.tan@live.com>
2023-04-04 16:36:36 +0800

    moved parkingaccuracy into HighscoreManager

--- My project/Assets/Scripts/HighscoreManager.cs
+++ My project/Assets/Scripts/HighscoreManager.cs
@@ -7,14 +7,14 @@ using TMPro;
 
 public class HighscoreManager : MonoBehaviour
 {
-    //TODO: bring merge parkingaccuracy script into here
     [SerializeField] private GameObject highscoreRowPrefab;
     [SerializeField] private Transform highscore;
     [SerializeField] private List<Dictionary<string, string>> currentHighscores;
     [SerializeField] private ColliderCheck colliderCheck;
-    [SerializeField] private ParkingAccuracy parkingAccuracy;
     [SerializeField] private LevelManager levelManager;
+    [SerializeField] private BoxCollider carCollider;
     [SerializeField] private TextMeshPro timeValueText, collisionValueText, accuracyValueText, timeScoreText, collisionScoreText, accuracyScoreText, totalScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,31 +27,68 @@ public class HighscoreManager : MonoBehaviour
         
     }
 
-    
-    public void SaveHighScore(float time) {
+    public void OnParked(float time) {
+        SaveHighScore(time);
+    }
+
+    // calculate the overlap volume of two box colliders
+    private float CalculateOverlapVolume(BoxCollider box1, BoxCollider box2)
+    {
+        //Debug.Log(box1.name);
+        //Debug.Log(box2.name);
+        Vector3 overlapSize = Vector3.Min(box1.bounds.max, box2.bounds.max) - Vector3.Max(box1.bounds.min, box2.bounds.min);
+        overlapSize = Vector3.Max(overlapSize, Vector3.zero);
+        return overlapSize.x * overlapSize.y * overlapSize.z;
+    }
+
+    private float getAccuracyPercentage()
+    {
+        BoxCollider parkingTrigger = GameObject.FindGameObjectWithTag("ParkingSpot").GetComponent<BoxCollider>();
+;
+        // if (parkingTrigger == null)
+        //     parkingTrigger = GameObject.FindGameObjectWithTag("ParkingSpot").GetComponent<BoxCollider>();
+
+        float overlapVolume = CalculateOverlapVolume(carCollider, parkingTrigger);
+        float carVolume = carCollider.bounds.size.x * carCollider.bounds.size.z * carCollider.bounds.size.y;
+        return overlapVolume / carVolume * 100f;
+        //Debug.Log("Parking accuracy: " + accuracyPercentage.ToString("F2") + "%");
+        //return accuracyPercentage;
+    }
+
+    private float calculateScore(float time) {
         float timeScore,collisionScore;
 
-        //get values
-        int numberOfCollisions = colliderCheck.getNumberOfCollisions();
-        float parkingAccuracyPercentage = parkingAccuracy.getAccuracyPercentage();
-
-        //reset values
-        colliderCheck.resetNumberOfCollisions();
-        parkingAccuracy.resetAccuracyPercentage();
-      
         if(time<60) timeScore = 1000;
         else if(time<120) timeScore = 800;
         else if(time<250) timeScore = 400;
         else if(time<500) timeScore = 200;
         else timeScore = 0;
 
+        int numberOfCollisions = colliderCheck.getNumberOfCollisions();
+        colliderCheck.resetNumberOfCollisions();
+
         if(numberOfCollisions<1) collisionScore = 1500;
         else if(numberOfCollisions<4) collisionScore = 1100;
         else if(numberOfCollisions <8) collisionScore = 700;
         else collisionScore = 0;
-    
+
+        float parkingAccuracyPercentage = getAccuracyPercentage();
         float parkingScore =  parkingAccuracyPercentage*20;
-        float score = parkingScore + collisionScore + timeScore;
+        float total = parkingScore + collisionScore + timeScore;
+
+        timeValueText.text = time.ToString("F2");
+        timeScoreText.text = timeScore.ToString();
+        collisionValueText.text = numberOfCollisions.ToString();
+        collisionScoreText.text = collisionScore.ToString();
+        accuracyValueText.text = parkingAccuracyPercentage.ToString("F2");
+        accuracyScoreText.text = parkingScore.ToString("F0");
+        totalScoreText.text = total.ToString("F0");
+
+        return total;
+    }
+
+    private void SaveHighScore(float time) {
+        float score = calculateScore(time);
 
         Dictionary<string, string> currentSession = new Dictionary<string, string>(); // dictionary kv pair to store level highscore)
         currentSession.Add("name", levelManager.playerName);
@@ -77,13 +114,7 @@ public class HighscoreManager : MonoBehaviour
 
         Debug.Log("Loading high score");
         
-        timeValueText.text = time.ToString("F2");
-        timeScoreText.text = timeScore.ToString();
-        collisionValueText.text = numberOfCollisions.ToString();
-        collisionScoreText.text = collisionScore.ToString();
-        accuracyValueText.text = parkingAccuracyPercentage.ToString("F2");
-        accuracyScoreText.text = parkingScore.ToString("F0");
-        totalScoreText.text = score.ToString("F0");
+        
         DisplayHighScore();
     }
 
